43bc7162facba5672de9ef66c5a43ce6e59bbd1c
Nikola Radovanovic <nikolalazarevac94@gmail.com>
2026-03-31 02:26:22 +0200

    add login and register

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Home from "./pages/Home.jsx";
 import Checkout from "./pages/Checkout.jsx";
 import Auth from "./pages/Auth.jsx";
 import Navbar from "./components/Navbar.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
